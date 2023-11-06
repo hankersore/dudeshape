33ff8d61474d50a64c67ee77983044905ed70400
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-06 13:42:05 +0500

    little changes

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,6 +3,10 @@ import Container from './Container';
 import Title from './Title';
 import Button from './Button';
 import showcase from '../assets/showcase.png';
+import showcase2 from '../assets/showcase2.jpeg';
+import showcase3 from '../assets/showcase3.jpeg';
+import showcase4 from '../assets/showcase4.jpeg';
+
 
 export default function Showcase () {
     const slides = [
@@ -18,7 +22,7 @@ export default function Showcase () {
               <Title h1='Get Your Best Cosmetic Products Here' p="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"/>
                 <form className='form'>
                         <input className='search2' type="email" placeholder='Search...'/>
-                        <Button name={<i class="fa-solid fa-magnifying-glass"></i>} backgroundColor= '#402F2C' textColor = '#FFF'/>
+                        <Button name={<i className="fa-solid fa-magnifying-glass"></i>} backgroundColor= '#402F2C' textColor = '#FFF'/>
                     </form>
                     <Button name='Shop Now' backgroundColor= '#402F2C' textColor = '#FFF' className="btn2"/>
               </Container>
@@ -26,4 +30,43 @@ export default function Showcase () {
                <img src={showcase} alt="" />
         </div>
     )
-}
+} 
+
+// import React, { useState, useEffect } from "react";
+
+// const Showcase = () => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const images = [
+//             '../assets/showcase.png',
+//             '../assets/showcase2.jpeg',
+//             '../assets/showcase3.jpeg',
+//             '../assets/showcase4.jpeg',
+//   ];
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentIndex((prevIndex) =>
+//         prevIndex === images.length - 1 ? 0 : prevIndex + 1
+//       );
+//     }, 2000);
+
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, [images.length]);
+
+//   return (
+//       <div className="image-slider">
+//       {images.map((image, index) => (
+//           <img
+//             key={index}
+//             src={image}
+//             alt={`Image ${index}`}
+//             style={{ display: index === currentIndex ? "block" : "none" }}
+//           />
+//         ))}
+//       </div>
+//   );
+// };
+
+// export default Showcase;
